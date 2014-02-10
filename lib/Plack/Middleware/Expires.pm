d985889900f0bb90707b1cb1d41bc523fea3c1de
Masahiro Nagano <kazeburo@gmail.com>
2014-02-10 13:05:05 +0900

    Checking in changes prior to tagging of version 0.05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 04d11f7..bfc6404 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Plack::Middleware::Expires
    
     {{$NEXT}}
    
    +0.05 2014-02-10T04:04:58Z
    +
    +        - accept code ref for content_type attribut (Thanks leedo)
    +
     0.04 2013-08-23T05:55:00Z
    
             - requires 'HTTP::Date' and 'HTTP::Status'. no requires LWP

--- lib/Plack/Middleware/Expires.pm
+++ lib/Plack/Middleware/Expires.pm
@@ -9,7 +9,7 @@ use Plack::Util::Accessor qw( content_type expires );
 use HTTP::Status qw//;
 use HTTP::Date;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 sub calc_expires {
     my ( $expires, $modified, $access ) = @_;
